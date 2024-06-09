4a82429590038704a39b28247b4e9dff6ed7fa7b
Bowen Zhu <48893229+invisi-splat@users.noreply.github.com>
2024-06-09 22:31:37 +0100

    Finish looks of overlay

--- src/routes/overlay/display/+page.svelte
+++ src/routes/overlay/display/+page.svelte
@@ -1,9 +1,49 @@
-<div class="text-white text-xl green-baize w-full h-10">
-Aoaidjaowijd
+<script lang="ts">
+    let something = true;
+
+    setTimeout(() => {
+        something = false;
+    }, 2000)
+    setTimeout(() => {
+        something = true;
+    }, 6000)
+</script>
+
+<div class="text-white text-2xl font-light h-14 flex items-center justify-center relative">
+    <div class="w-full flex items-center h-full z-5">
+        <div class="flex-grow flex justify-between items-center h-10 pl-20 pr-6 basis-0">
+            <div>Kasidit Lauhabutr</div>
+            <div>72</div>
+        </div>
+        <div class="h-full border-4 flex items-center justify-evenly border-black w-[10%] blue-baize">
+            <span class="">2</span>
+            <span>(11)</span>
+            <span>4</span>
+        </div>
+        <div class="flex-grow flex justify-between items-center text-right h-10 pr-20 pl-6 basis-0">
+            <div>38</div>
+            <div class="text-yellow-200">Om Goswamy</div>
+        </div>
+    </div>
+    <!-- just the background -->
+    <div class="green-baize w-full absolute -z-10 h-10"></div>
+    <div class="w-full absolute text-black -z-20 h-12 { something ? 'top-2' : 'top-12' } flex justify-center pl-20 pr-20 transition-all duration-1000">
+        <div class="hidden h-10 -flex w-2/5 bg-gray-300 bg-opacity-50 items-center">
+            <span class="ml-4">41 Ahead</span>
+            <span class="ml-6">35 Remaining</span>
+        </div>
+        <div class="flex h-10 w-1/4 bg-gray-300 bg-opacity-50 items-center justify-center">
+            0:21 Average shot time 1:42
+        </div>
+    </div>
 </div>
 
 <style>
 .green-baize {
     background: var(--green-baize);
+}
+
+.blue-baize {
+    background: var(--blue-baize);
 }
 </style>
