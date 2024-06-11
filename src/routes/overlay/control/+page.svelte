4036f6f3f83215be6200bca0b66dcdbc1146d89d
Bowen Zhu <48893229+invisi-splat@users.noreply.github.com>
2024-06-11 23:54:21 +0100

    Implement a bunch of stuff and get p2p working

--- src/routes/overlay/control/+page.svelte
+++ src/routes/overlay/control/+page.svelte
@@ -1,29 +1,103 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import ControlBall from "./controlBall.svelte";
+    import Peer from "peerjs";
+
+    let showOverlaySettings = true;
+    let p2pid = localStorage["p2pid"] ?? "";
+    let conn: any = null;
+    let errorMessage = "";
+    let statusMessage = "";
+    let successMessage = "";
+    let peer = new Peer({ debug: 3 });
+
+    const emptyPlayer = {
+        name: "",
+        framesWon: 0,
+        currentScore: 0,
+        currentBreak: [],
+        averageShotTime: 0,
+        foulsCommitted: 0,
+        successfulSafeties: 0,
+        failedSafeties: 0,
+        successfulLongpots: 0,
+        failedLongpots: 0
+    }
+
+    const emptyScoreboard = {
+        player: Array(2).fill(null).map(() => {return structuredClone(emptyPlayer)}),
+        bestOf: 1,
+        activeTurn: 0,
+        onRed: false,
+    }
+
+    let scoreboardInfoStr = localStorage["scoreboardInfo"] ?? JSON.stringify(structuredClone(emptyScoreboard));
+    let scoreboardInfo = JSON.parse(scoreboardInfoStr);
+
+    console.log(scoreboardInfo);
+
+    const connectToPeer = () => {
+        errorMessage = "";
+        statusMessage = "Connecting...";
+        successMessage = "";
+        conn = peer.connect(p2pid);
+
+        conn.on("open", () => {
+            errorMessage = "";
+            statusMessage = "";
+            successMessage = "Connected successfully!";
+        });
+
+        localStorage["p2pid"] = p2pid;
+
+        setTimeout(() => {
+            statusMessage = "";
+            if (!conn.open) {
+                conn.close();
+                errorMessage = "Connection timed out.";
+            }
+        }, 5000);
+    }
+
+    const resetAll = () => {
+        localStorage.removeItem("scoreboardInfo");
+        scoreboardInfo = structuredClone(emptyScoreboard);
+    }
+
+    const saveMatchDetails = () => {
+        showOverlaySettings = !showOverlaySettings;
+    }
+
+    $: {
+        localStorage["scoreboardInfo"] = JSON.stringify(scoreboardInfo);
+        if (conn !== null) {
+            conn.send(scoreboardInfo);
+        }
+    }
+
+    onMount(() => {
+        // connectToPeer();
+    })
 </script>
 
 <div class="text-3xl text-white pt-5 green-baize w-svw min-h-svh">
+    {#each {length: 2} as _, i}
     <div class="font-bold px-5 mb-1 space-y-1">
         <div class="flex justify-between items-end">
-            <div class="max-w-[70%] text-nowrap overflow-hidden overflow-ellipsis">Kasidit Lauhabutr</div>
-            <div><span class="text-5xl font-bold text-active-yellow">10</span><span class="pl-2 text-lg font-light text-white">[4] 72</span></div>
+            <div class="max-w-[70%] text-nowrap overflow-hidden overflow-ellipsis">{ scoreboardInfo.player[i].name }</div>
+            <div><span class="text-5xl font-bold { scoreboardInfo.activeTurn === i ? 'text-active-yellow' : 'opacity-20' }">{ scoreboardInfo.player[i].currentBreak.reduce((a, b) => a + b, 0) }</span><span class="pl-2 text-lg font-light text-white">[{ scoreboardInfo.player[i].framesWon }] { scoreboardInfo.player[i].currentScore }</span></div>
         </div>
     </div>
-    <div class="font-light px-5 mb-4 space-y-1">
-        <div class="flex justify-between items-end">
-            <div class="max-w-[70%] text-nowrap overflow-hidden overflow-ellipsis">Om Goswamy</div>
-            <div><span class="text-5xl font-bold opacity-20">0</span><span class="pl-2 text-lg font-light text-white">[2] 38</span></div>
-        </div>
-    </div>
+    {/each}
     <div class="flex flex-col items-center space-y-5">
         <div class="grid grid-cols-3 gap-4 font-medium">
-            <button class="col-span-3 active:opacity-50 flex justify-center"><ControlBall value={1}></ControlBall></button>
-            <button class="col-span-1 active:opacity-50 justify-center"><ControlBall value={2}></ControlBall></button>
-            <button class="col-span-1 active:opacity-50 justify-center"><ControlBall value={3}></ControlBall></button>
-            <button class="col-span-1 active:opacity-50 justify-center"><ControlBall value={4}></ControlBall></button>
-            <button class="col-span-1 active:opacity-50 justify-center"><ControlBall value={5}></ControlBall></button>
-            <button class="col-span-1 active:opacity-50 justify-center"><ControlBall value={6}></ControlBall></button>
-            <button class="col-span-1 active:opacity-50 justify-center"><ControlBall value={7}></ControlBall></button>
+            <button on:click={ () => { scoreboardInfo.player[scoreboardInfo.activeTurn].currentBreak.push(1); scoreboardInfo.player[scoreboardInfo.activeTurn].currentScore += 1; scoreboardInfo = scoreboardInfo; } } class="col-span-3 active:opacity-50 flex justify-center"><ControlBall value={1}></ControlBall></button>
+            <button on:click={ () => { scoreboardInfo.player[scoreboardInfo.activeTurn].currentBreak.push(2); scoreboardInfo.player[scoreboardInfo.activeTurn].currentScore += 2; scoreboardInfo = scoreboardInfo; } } class="col-span-1 active:opacity-50 justify-center"><ControlBall value={2}></ControlBall></button>
+            <button on:click={ () => { scoreboardInfo.player[scoreboardInfo.activeTurn].currentBreak.push(3); scoreboardInfo.player[scoreboardInfo.activeTurn].currentScore += 3; scoreboardInfo = scoreboardInfo; } } class="col-span-1 active:opacity-50 justify-center"><ControlBall value={3}></ControlBall></button>
+            <button on:click={ () => { scoreboardInfo.player[scoreboardInfo.activeTurn].currentBreak.push(4); scoreboardInfo.player[scoreboardInfo.activeTurn].currentScore += 4; scoreboardInfo = scoreboardInfo; } } class="col-span-1 active:opacity-50 justify-center"><ControlBall value={4}></ControlBall></button>
+            <button on:click={ () => { scoreboardInfo.player[scoreboardInfo.activeTurn].currentBreak.push(5); scoreboardInfo.player[scoreboardInfo.activeTurn].currentScore += 5; scoreboardInfo = scoreboardInfo; } } class="col-span-1 active:opacity-50 justify-center"><ControlBall value={5}></ControlBall></button>
+            <button on:click={ () => { scoreboardInfo.player[scoreboardInfo.activeTurn].currentBreak.push(6); scoreboardInfo.player[scoreboardInfo.activeTurn].currentScore += 6; scoreboardInfo = scoreboardInfo; } } class="col-span-1 active:opacity-50 justify-center"><ControlBall value={6}></ControlBall></button>
+            <button on:click={ () => { scoreboardInfo.player[scoreboardInfo.activeTurn].currentBreak.push(7); scoreboardInfo.player[scoreboardInfo.activeTurn].currentScore += 7; scoreboardInfo = scoreboardInfo; } } class="col-span-1 active:opacity-50 justify-center"><ControlBall value={7}></ControlBall></button>
         </div>
         <div class="grid grid-cols-4 gap-3 text-2xl">
             <button class="w-18 active:opacity-50 p-2 col-span-1 blue-gradient">Game</button>
@@ -45,7 +119,42 @@
             seconds
         </div>
         <div class="flex justify-end w-full pr-5">
-            <button class="w-18 active:opacity-50 p-2 purple-gradient text-xl">Overlay settings</button>
+            <button on:click={() => { showOverlaySettings = true; }} class="w-18 active:opacity-50 p-2 purple-gradient text-xl">Overlay settings</button>
+        </div>
+    </div>
+</div>
+<div class="{ showOverlaySettings ? 'flex' : 'hidden' } justify-center items-center w-dvw h-dvh absolute top-0 left-0 z-20 backdrop-blur-md">
+    <div class="w-[80%] max-h-[90%] overflow-y-scroll bg-gray-200 opacity-90 p-3 text-2xl space-y-2">
+        Connection pin
+        <br>
+        <div class="w-full flex justify-center">
+            <input bind:value={ p2pid } type="text" class="w-[90%] p-2 text-2xl bg-transparent border-2 border-black" />
+        </div>
+        <div class="w-full flex justify-end">
+            <button on:click={ connectToPeer } class="p-2 purple-gradient text-2xl text-white active:opacity-50">Connect</button>
+        </div>
+        <span class="text-red-400 font-light text-xl">{ errorMessage }</span>
+        <span class="text-gray-600 font-light text-xl">{ statusMessage }</span>
+        <span class="text-green-400 font-light text-xl">{ successMessage }</span>
+        <br>
+        Player 1 name
+        <br>
+        <div class="w-full flex justify-center mb-10">
+            <input bind:value={ scoreboardInfo.player[0].name } type="text" class="w-[90%] p-2 text-2xl bg-transparent border-2 border-black" />
+        </div>
+        Player 2 name
+        <br>
+        <div class="w-full flex justify-center">
+            <input bind:value={ scoreboardInfo.player[1].name } type="text" class="w-[90%] p-2 text-2xl bg-transparent border-2 border-black" />
+        </div>
+        <div class="w-full flex justify-center gap-x-2 items-center px-5">
+            <span class="whitespace-nowrap">Race to</span>
+            <input bind:value={ scoreboardInfo.bestOf } type="number" pattern="[0-9]*"  class="w-[70%] p-2 text-2xl bg-transparent border-2 border-black" />
+        </div>
+        <br>
+        <div class="w-full flex justify-end gap-x-3">
+            <button on:click={ resetAll } class="p-2 orange-gradient text-2xl text-white active:opacity-50">Reset all</button>
+            <button on:click={ saveMatchDetails } class="p-2 blue-gradient text-2xl text-white active:opacity-50">Close</button>
         </div>
     </div>
 </div>
--- src/routes/overlay/display/+page.svelte
+++ src/routes/overlay/display/+page.svelte
@@ -1,30 +1,83 @@
 <script lang="ts">
+    import { browser } from "$app/environment";
+    import Peer from "peerjs";
+
     let something = true;
 
-    setTimeout(() => {
-        something = false;
-    }, 2000)
-    setTimeout(() => {
-        something = true;
-    }, 6000)
+    let p2pid = ""; 
+    let showConnectionPane: boolean; 
+    let peer: Peer;
+    if (browser) {
+        p2pid = localStorage["p2pid"] ?? "";
+    }
 
-    let showConnectionPane = true;
+    const emptyPlayer = {
+        name: "",
+        framesWon: 0,
+        currentScore: 0,
+        currentBreak: [],
+        averageShotTime: 0,
+        foulsCommitted: 0,
+        successfulSafeties: 0,
+        failedSafeties: 0,
+        successfulLongpots: 0,
+        failedLongpots: 0
+    }
+
+    const emptyScoreboard = {
+        player: Array(2).fill(null).map(() => {return structuredClone(emptyPlayer)}),
+        bestOf: 1,
+        activeTurn: 0,
+        onRed: false,
+    }
+
+    let scoreboardInfo = structuredClone(emptyScoreboard);
+
+    showConnectionPane = true;
+
+    if (p2pid === "") {
+        peer = new Peer(Math.random().toString(36).slice(2, 10), { debug: 3 });
+    } else {
+        peer = new Peer(p2pid, { debug: 3 });
+    }
+
+    peer.on("open", id => {
+        p2pid = id;
+        localStorage["p2pid"] = p2pid;
+    })
+
+    peer.on("connection", conn => {
+        showConnectionPane = false;
+        conn.on("data", data => {
+            scoreboardInfo = data; // yes it fucking is
+        })
+    })
+
+    peer.on("disconnected", conn => {
+        showConnectionPane = true;
+    })
 </script>
+
+<svelte:window on:keydown|preventDefault={(e) => {
+    if (e.key === "m") {
+        showConnectionPane = !showConnectionPane;
+    }
+}}></svelte:window>
 
 <div class="text-white text-2xl font-light h-14 flex items-center justify-center relative">
     <div class="w-full flex items-center h-full z-5">
         <div class="flex-grow flex justify-between items-center h-10 pl-20 pr-6 basis-0">
-            <div>Kasidit Lauhabutr</div>
-            <div>72</div>
+            <div class="{ scoreboardInfo.activeTurn === 0 ? 'text-yellow-200' : ''} ">{ scoreboardInfo.player[0].name }</div>
+            <div>{ scoreboardInfo.player[0].currentScore }</div>
         </div>
         <div class="h-full border-4 flex items-center justify-evenly border-black w-[10%] blue-baize">
-            <span class="">2</span>
-            <span>(11)</span>
-            <span>4</span>
+            <span class="">{ scoreboardInfo.player[0].framesWon }</span>
+            <span>({ scoreboardInfo.bestOf })</span>
+            <span>{ scoreboardInfo.player[1].framesWon }</span>
         </div>
         <div class="flex-grow flex justify-between items-center text-right h-10 pr-20 pl-6 basis-0">
-            <div>38</div>
-            <div class="text-yellow-200">Om Goswamy</div>
+            <div>{ scoreboardInfo.player[1].currentScore }</div>
+            <div class={ scoreboardInfo.activeTurn === 1 ? 'text-yellow-200' : ''}>{ scoreboardInfo.player[1].name }</div>
         </div>
     </div>
     <!-- just the background -->
@@ -34,7 +87,7 @@
             <span class="ml-4">41 Ahead</span>
             <span class="ml-6">35 Remaining</span>
         </div>
-        <div class="flex h-10 w-1/4 bg-gray-300 bg-opacity-50 items-center justify-center">
+        <div class="flex h-10 min-w-[25%] bg-gray-300 bg-opacity-50 items-center justify-center">
             0:21 Average shot time 1:42
         </div>
     </div>
@@ -45,8 +98,9 @@
         <br>
         Head to the <a href="/overlay/control" class="underline text-yellow-500">remote control</a> page and enter the following code in settings to connect the remote:
         <div class="w-full flex justify-center my-10">
-            <span class="text-3xl font-mono">1239hda0</span>
+            <span class="text-3xl font-mono">{ p2pid }</span>
         </div>
+        (Press <span class="font-mono">m</span> to toggle this menu.)
     </div>
 </div>
 
